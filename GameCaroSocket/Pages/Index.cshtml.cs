4392f238d3f0fc77e1cce484a55af437acb73c7c
sugar <ladyfirst0109@gmail.com>
2020-11-26 16:13:25 +0700

    pc5

--- GameCaroSocket/Pages/Index.cshtml.cs
+++ GameCaroSocket/Pages/Index.cshtml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -10,15 +13,49 @@ namespace GameCaroSocket.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        [Obsolete]
+        private readonly IHostingEnvironment _host;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        [Obsolete]
+        public IndexModel(IHostingEnvironment host)
         {
-            _logger = logger;
+            _host = host;
         }
 
+        [Obsolete]
         public void OnGet()
         {
+
+            string ip_log_path = Path.Combine(_host.WebRootPath, "lib", "log");
+            string ip = Request.HttpContext.Connection.RemoteIpAddress.ToString();
+            string fileName = System.IO.Path.Combine(ip_log_path, "log.txt");
+
+            if (!System.IO.File.Exists(fileName))
+            {
+                System.IO.File.WriteAllText(fileName, "");
+            }
+
+            try
+            {
+                int line = System.IO.File.ReadAllLines(fileName).Where(x=>!x.Contains("------------")).Count();
+                using (FileStream stream = new FileStream(fileName, FileMode.Append))
+                {
+                    if (line == 0)
+                    {
+                        line = 1;
+                    }
+                    else
+                    {
+                        line += 1;
+                    }
+
+                    byte[] bytes = Encoding.UTF8.GetBytes(line + ".\t" + ip + '\t' + DateTime.Now + "\n-----------------------------------------------\n");;
+                    stream.Write(bytes, 0, bytes.Length);
+                }
+            }
+            catch (IOException)
+            {
+            }
 
         }
     }
